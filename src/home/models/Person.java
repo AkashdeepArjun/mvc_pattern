80e95d51e4dcdd57f2be28f2964970a6a9559b71
AkashdeepArjun <wadhwaarjun007@gmail.com>
2022-04-26 03:22:56 -0700

    person class data

---
+++ src/home/models/Person.java
@@ -0,0 +1,26 @@
+package home.models;
+
+ public class Person{
+
+    private String name;
+    private int age;
+
+
+    public void setName(String name){
+        this.name=name;
+    }
+
+    public String getName(){
+
+        return this.name;
+    }
+
+    public void setAge(int age){
+        this.age=age;
+    }
+
+    public int getAge(){
+        return this.age;
+    }
+
+}
